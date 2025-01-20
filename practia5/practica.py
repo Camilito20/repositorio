c87e43ea30eff6bbf69c8f86b8c7b1a71595d058
Camilito20 <camitopazmino@gmail.com>
2025-01-20 20:57:48 +0700

    menu sin ejercicio

---
+++ practia5/practica.py
@@ -0,0 +1,23 @@
+def main():
+    print('----- Меню -----')
+    print('1. Вычислить х в ln(1+x)')
+    print('2. Вычислить х в ln(1-x)')
+    print('3. Выходить')
+    respuesta = input('выбирайте вариант(1-3):')
+
+    if respuesta == '1':
+        print('первое задание')
+
+    elif respuesta == '2':
+        print('второе задание')
+
+    elif respuesta == '3':
+        print('Выход....')
+
+    else:
+        print("\nВыбидайте вариант(1, 2 или 3)\n")
+        return main()
+
+
+if __name__ == "__main__":
+    main()
