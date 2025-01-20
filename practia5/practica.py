1b05d07a126a9c5286c88af0e59b4dc69876b353
Camilito20 <camitopazmino@gmail.com>
2025-01-21 01:46:01 +0700

    Menu

--- practia5/practica.py
+++ practia5/practica.py
@@ -6,7 +6,7 @@ def main():
     respuesta = input('выбирайте вариант(1-3): ')
 
     if respuesta == '1':
-        return exersice1()
+        print('1')
 
     elif respuesta == '2':
         print('второе задание')
@@ -17,20 +17,6 @@ def main():
     else:
         print("\nВыбидайте вариант(1, 2 или 3)\n")
         return main()
-
-
-def exersice1():
-    x = float(input('\nнаптшите число x(-1 < x <= 1): '))
-    N = 100
-    if (-1 < x <= 1) == False:
-        raise ValueError("число должен -1 < x <= 1)")
-
-    suma = 0
-    for n in range(1, N + 1):
-        ecuacion = ((-1)**(n + 1) * (x ** n) / (n))
-        suma += ecuacion
-
-    print(f'Решении ln(x + 1) = {suma}')
 
 
 if __name__ == "__main__":
